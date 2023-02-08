7ca27a8962e09b8826a7b16a6ec5d8ac82f15572
monicah79 <monniewanjema@gmail.com>
2023-02-08 22:39:03 +0300

    Add reducer and action

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Categories from './Components/Categories';
 import Books from './Components/Books';
 import Header from './Components/Header';
@@ -7,12 +7,14 @@ import Header from './Components/Header';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/Categories" element={<Categories />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/Categories" element={<Categories />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
